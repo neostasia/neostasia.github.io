7585ebbd9d4740a8efff18e602b2e3b531a4f192
mache102 <bee365go@gmail.com>
2023-07-23 11:49:16 +0800

    css reorg and more homepage changes

---
+++ assets/js/blog_swiper.js
@@ -0,0 +1,34 @@
+$(".blog-active").slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  
